e2ad97ac2b5e078a4e0a6875132afea50198a3d7
madmax-op <vivekaman742@gmail.com>
2025-07-10 23:09:11 +0530

    Remove Duplicates in a String

---
+++ Recursion_Basics/prog11.java
@@ -0,0 +1,19 @@
+class prog11 {
+    public static void removeDuplicates (String str , int index , StringBuilder newStr , boolean[] map){
+        if (index==str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char currentChar = str.charAt(index);
+        if(map[currentChar-'a']==true){
+            removeDuplicates(str, index+1, newStr, map);
+        }else{
+            map[currentChar-'a']=true;
+            removeDuplicates(str, index+1, newStr.append(currentChar), map);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "vvivekaaman";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+}
