38702af44916995ec7ea6b29c5143a30f027682e
madmax-op <vivekaman742@gmail.com>
2025-07-21 23:05:33 +0530

    Author

--- Divide&Conquer/prog2.java
+++ Divide&Conquer/prog2.java
@@ -17,7 +17,6 @@ public class prog2 {
     public static int partition(int arr[], int si , int ei){
         int pivot = arr[ei];
         int i = si - 1;
-
         for (int j = si; j < ei; j++) {
             if (arr[j] <= pivot) {
                 i++;
