7f97479a8f6a56f8ad5d2ba1e6299d3a9217e6dc
madmax-op <vivekaman742@gmail.com>
2025-08-09 19:01:19 +0530

    N Queens

--- BackTracking/prog4.java
+++ BackTracking/prog4.java
@@ -1,1 +1,14 @@
 // N Queens
+public class prog4 {
+    public static void nQueens (char board[][], int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        //coloumn loop
+        for (int j=0; j<board.length;j++){
+            board[row][j]=
+        }
+    }
+}
