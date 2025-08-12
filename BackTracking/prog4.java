c4d244b59769401a7935a8fa577d8ce72a9aa70a
madmax-op <vivekaman742@gmail.com>
2025-08-12 23:01:23 +0530

    N Queens

--- BackTracking/prog4.java
+++ BackTracking/prog4.java
@@ -8,7 +8,30 @@ public class prog4 {
         }
         //coloumn loop
         for (int j=0; j<board.length;j++){
-            board[row][j]=
+            board[row][j]='Q';
+            nQueens(board , row+1); // function call
+            board[row][j]='.';// backtracking step
         }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("----- chess board -----");
+        for (int i=0; i<board.length;i++){
+            for(int j=0; j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n=2;
+        char board [][]= new char [n][n];
+        //initialize
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='x';
+            }
+        }
+        nQueens(board, 0);
     }
 }
